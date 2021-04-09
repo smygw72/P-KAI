af7c316898feb492112d23c9d83d009f31a899b1
smygw72 <smygw72@gmail.com>
2021-04-09 16:55:51 +0900

    Update for dataset scalability

--- smygw/utils/make_pair.py
+++ smygw/utils/make_pair.py
@@ -14,10 +14,25 @@ def main():
     print(len(pair_list))
 
     with open('./smygw/annotation/all_pair.csv', mode='a') as f:
+        reader = csv.reader(f)
+        old_list = []
+        for row in reader:
+            old_id1 = row[0]
+            old_id2 = row[1]
+            old_row = {'id1': old_id1, 'id2': old_id2}
+            old_list.append(old_row)
+
         writer = csv.writer(f)
-        writer.writerow(['id1', 'id2', 'label'])  # header
+
+        if len(old_list) > 0:
+            writer.writerow(['id1', 'id2', 'label'])  # header
+
         for pair in pair_list:
-            writer.writerow([pair["id1"], pair["id2"], 'X'])
+            new_id1 = pair["id1"]
+            new_id2 = pair["id2"]
+            new_row = {'id1': new_id1, 'id2': new_id2}
+            if not new_row in old_list:
+                writer.writerow([new_id1, new_id2, 'X'])
 
 
 if __name__ == "__main__":
