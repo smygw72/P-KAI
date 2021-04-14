3e9ddc2f7cf050c7649840fa37c4474edd9668c0
smygw72 <smygw72@gmail.com>
2021-04-14 23:13:12 +0900

    fix bug in to_csv and add args option

--- smygw/utils/annotate.py
+++ smygw/utils/annotate.py
@@ -1,25 +1,46 @@
+import argparse
 import sys
 import csv
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import networkx as nx
 
 annotation_file = './smygw/annotation/all_pair.csv'
 # annotation_file = './smygw/test.csv'
 
+miyagawa_range = (1, 145)
+kanaishi_range = (146, 290)
+kuroki_range = (291, 435)
+
+
+def args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--miyagawa', action="store_true")
+    parser.add_argument('--kanaishi', action="store_true")
+    parser.add_argument('--kuroki', action="store_true")
+    return parser.parse_args()
+
 
 def main():
+    args = args_parser()
     edges = []  # list of (start, end, weight)
-    unannotated_nodes = []
+    unannotated_idxs = []
 
     with open(annotation_file, mode='r') as f:
         reader = csv.reader(f)
 
-        for row in reader:
+        for i, row in enumerate(reader):
             if row == ('id1', 'id2', 'label'):
                 continue
             if row[2] == 'X':
-                unannotated_nodes.append((row[0], row[1]))
+                if (args.miyagawa is True) and ((i < miyagawa_range[0]) or (i > miyagawa_range[1])):
+                    continue
+                elif (args.kanaishi is True) and ((i < kanaishi_range[0]) or (i > kanaishi_range[1])):
+                    continue
+                elif (args.kuroki is True) and ((i < kuroki_range[0]) or (i > kuroki_range[1])):
+                    continue
+                else:
+                    unannotated_idxs.append(i - 1)
             else:
                 label = row[2]
                 if label == '1':
@@ -36,23 +57,27 @@ def main():
     # plt.axis("off")
     # plt.show()
 
-    if len(unannotated_nodes) == 0:
+    if len(unannotated_idxs) == 0:
         print('There are no pairs')
         sys.exit(0)
 
     df = pd.read_csv(annotation_file)
     print("Input 1/0/-1\n")
-    for i, node in enumerate(unannotated_nodes):
-        answer = question(graph, node[0], node[1])
+
+    for idx in unannotated_idxs:
+        node1 = df.iloc[idx, 0]
+        node2 = df.iloc[idx, 1]
+
+        answer = question(graph, node1, node2)
         if answer is not None:
-            df.iloc[i, 2] = str(answer)
+            df.iloc[idx, 2] = str(answer)
             continue
-        answer = question(graph, node[1], node[0])
+        answer = question(graph, node2, node1)
         if answer is not None:
-            df.iloc[i, 2] = str(-1 * answer)
+            df.iloc[idx, 2] = str(-1 * answer)
             continue
 
-    df.to_csv(annotation_file)
+    df.to_csv(annotation_file, index=False)
 
 
 def question(graph, id1, id2):
@@ -65,9 +90,6 @@ def question(graph, id1, id2):
         print(f'{id1}, {id2} (length: {length})')
         return int(input())
     return None
-
-
-
 
 
 if __name__ == "__main__":
