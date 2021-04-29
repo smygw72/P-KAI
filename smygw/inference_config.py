2ffc805b8ab347872bee34bf4f5ae6ae4e47aeab
smygw72 <smygw72@gmail.com>
2021-04-29 13:47:44 +0900

    fix previous path

--- smygw/inference_config.py
+++ smygw/inference_config.py
@@ -4,6 +4,6 @@ from addict import Dict
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', default='./smygw/inference_config.yaml')
-parser.add_argument('--path', default='../sounds/Z_-hWZetOS0.mp3')
+parser.add_argument('--path', default='../dataset/sounds/Z_-hWZetOS0.mp3')
 args = parser.parse_args()
 CONFIG = Dict(yaml.safe_load(open(args.config)))
--- smygw/utils/download.py
+++ smygw/utils/download.py
@@ -44,17 +44,19 @@ def download(row, output_dir):
 
 
 def main():
-    df = pd.read_csv('./smygw/youtube.csv', header=0)
+    csv_path = './smygw/youtube.csv'
+    sound_dir = '../dataset/sounds'
+
+    df = pd.read_csv(csv_path, header=0)
     df.drop_duplicates(subset='ID', inplace=True)
-    os.makedirs('../sounds', exist_ok=True)
+    os.makedirs(sound_dir, exist_ok=True)
 
     for i, row in df.iterrows():
         print(row)
-        output_dir = '../sounds'
-        data_path = f'{output_dir}/{row["ID"]}.mp3'
+        data_path = f'{sound_dir}/{row["ID"]}.mp3'
         if not os.path.isfile(data_path):
-            download(row, output_dir)
-            trim(row, output_dir)
+            download(row, sound_dir)
+            trim(row, sound_dir)
 
 
 if __name__ == "__main__":
