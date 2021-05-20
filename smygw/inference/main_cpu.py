1e81471b35abb6825fefe4ad03f8c0a2b09bb08c
smygw <smygw72@gmail.com>
2021-05-20 10:06:01 +0900

    copy main.py for cpu/gpu acceleration

--- smygw/inference/main_cpu.py
+++ smygw/inference/main_cpu.py
@@ -1,6 +1,8 @@
 import os
 import warnings
 import random
+from multiprocessing import Pool, cpu_count
+import time
 import numpy as np
 from tqdm import tqdm
 from mutagen.mp3 import MP3
@@ -12,6 +14,12 @@ from config import CONFIG
 from network.interface import get_model
 from utils.make_mfcc import spec_to_image, get_melspectrogram_db
 
+
+transform = transforms.Compose([
+    transforms.Resize((
+        CONFIG.common.input_size, CONFIG.common.input_size
+    )),
+])
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
@@ -36,12 +44,29 @@ def mutagen_length(path):
         return None
 
 
+def inference(model, file_path, i):
+    start_segment = i * CONFIG.inference.window_wid
+    spec = get_melspectrogram_db(
+        file_path, offset=start_segment,
+        duration=CONFIG.inference.window_wid
+    )
+    input_arr = spec_to_image(spec)
+    input_tensor = torch.from_numpy(
+        input_arr
+    ).float().unsqueeze(0).unsqueeze(0)
+    input_tensor = transform(input_tensor)
+    score = model(input_tensor.to(device)).to('cpu').item()
+    return score
+
+
 def main(*args, **kwargs):
+    start_time = time.time()
     warnings.filterwarnings('ignore')
     set_seed(CONFIG.inference.seed)
 
     # file_path = args.path
     file_path = './smygw/inference/test.mp3'
+
     file_name = os.path.splitext(os.path.basename(file_path))[0]
     length = mutagen_length(file_path)
 
@@ -54,44 +79,22 @@ def main(*args, **kwargs):
     model.load_state_dict(torch.load(model_path))
     model.eval()
 
-    inputs = torch.Tensor()
-    outputs = torch.Tensor()
-
     n_mfcc = int(length / CONFIG.inference.window_wid)
     with torch.no_grad():
-        for i in tqdm(range(n_mfcc)):
-            start_segment = i * CONFIG.inference.window_wid
-
-            spec = get_melspectrogram_db(
-                file_path, offset=start_segment,
-                duration=CONFIG.inference.window_wid
-            )
-            input_arr = spec_to_image(spec)  # 2D
-            input_tensor = torch.from_numpy(input_arr).unsqueeze(0)
-
-            transform = transforms.Compose([
-                transforms.Resize((
-                    CONFIG.common.input_size, CONFIG.common.input_size
-                )),
-            ])
-            input_tensor = transform(input_tensor)
-
-            inputs = torch.cat([inputs, input_tensor.unsqueeze(0)], dim=0)
-
-            if (len(inputs) == CONFIG.inference.batch_size) or (i == n_mfcc - 1):
-                inputs = inputs.to(device)
-                output = model(inputs).detach().to('cpu')
-                outputs = torch.cat([outputs, output], dim=0)
-                inputs = torch.Tensor()  # make empty
+        p = Pool(cpu_count())
+        args = [(model, file_path, i) for i in range(n_mfcc)]
+        outputs = p.starmap(inference, args)
 
     for i in range(len(outputs)):
         writer.add_scalar("timeline", outputs[i], i)
 
-    score_avg = torch.mean(outputs).item()
+    score_avg = sum(outputs) / len(outputs)
     print(f"average score: {score_avg}")
 
     writer.close()
-
+    end_time = time.time()
+    sec_per_frame = (end_time - start_time) / n_mfcc
+    print(f"elapsed time: {sec_per_frame}")
     return score_avg
 
 
