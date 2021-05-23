6817a43e603aef141636f55dbbc9125150b4e685
smygw72 <smygw72@gmail.com>
2021-05-23 04:59:16 +0000

    add resnet50

--- smygw/network/interface.py
+++ smygw/network/interface.py
@@ -2,5 +2,7 @@ from .resnet import get_resnet
 
 
 def get_model(arch):
-    if arch == 'resnet':
-        return get_resnet()
+    if arch == 'resnet34':
+        return get_resnet('34')
+    elif arch == 'resnet50':
+        return get_resnet('50')
