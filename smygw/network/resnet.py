1a8bdb9800d6e40f6890ef8387a879e7c6efc014
smygw72 <smygw72@gmail.com>
2021-05-23 04:58:34 +0000

    add resnet50

--- smygw/network/resnet.py
+++ smygw/network/resnet.py
@@ -1,10 +1,15 @@
 import torch.nn as nn
-from torchvision.models import resnet34
+from torchvision.models import resnet34, resnet50
 
 
-def get_resnet():
-    model = resnet34(pretrained=True)
-    model.fc = nn.Linear(512, 1)
+def get_resnet(n_layer):
+    if n_layer == '34':
+        model = resnet34(pretrained=True)
+        model.fc = nn.Linear(512, 1)
+    elif n_layer == '50':
+        model = resnet50(pretrained=True)
+        model.fc = nn.Linear(2048, 1)
+
     model.conv1 = nn.Conv2d(
         1, 64, kernel_size=(7, 7),
         stride=(2, 2),
