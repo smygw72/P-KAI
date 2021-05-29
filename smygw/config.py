64ef0b4c98b0c235d30daaa15854ed032d6d364c
smygw72 <smygw72@gmail.com>
2021-05-29 02:22:41 +0000

    refactoring with mlflow and hydra

--- smygw/config.py
+++ smygw/config.py
@@ -1,8 +1,28 @@
-import argparse
-import yaml
-from addict import Dict
+# copy from https://zenn.dev/kwashizzz/articles/ml-hydra-param
+import sys
+import os
+from hydra.experimental import compose, initialize_config_dir
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--config', default='./smygw/config.yaml')
-args = parser.parse_args()
-CONFIG = Dict(yaml.safe_load(open(args.config)))
+
+class Config():
+    """
+    hydraによる設定値の取得 (conf)
+    """
+    @staticmethod
+    def get_cnf():
+        """
+        設定値の辞書を取得
+        @return
+            cnf: OmegaDict
+        """
+        conf_dir = os.path.join(os.getcwd(), "smygw/")
+        if not os.path.isdir(conf_dir):
+            print(f"Can not find file: {conf_dir}.")
+            sys.exit(-1)
+
+        with initialize_config_dir(config_dir=conf_dir):
+            cnf = compose(config_name="config.yaml")
+            return cnf
+
+
+CONFIG = Config.get_cnf()
--- smygw/inference/main.py
+++ smygw/inference/main.py
@@ -9,17 +9,11 @@ from mutagen.mp3 import MP3
 import torch
 from torchvision import transforms
 from tensorboardX import SummaryWriter
+
 import _paths
 from config import CONFIG
 from network.interface import get_model
 from utils.make_mfcc import spec_to_image, get_melspectrogram_db
-
-
-transform = transforms.Compose([
-    transforms.Resize((
-        CONFIG.common.input_size, CONFIG.common.input_size
-    )),
-])
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
@@ -46,14 +40,18 @@ def mutagen_length(path):
 
 def cpu_inference(model, sound_path, i):
     start_segment = i * CONFIG.inference.window_wid
-    spec = get_melspectrogram_db(
-        sound_path, offset=start_segment,
-        duration=CONFIG.inference.window_wid
-    )
+    spec = get_melspectrogram_db(sound_path,
+                                 offset=start_segment,
+                                 duration=CONFIG.inference.window_wid)
     input_arr = spec_to_image(spec)
-    input_tensor = torch.from_numpy(
-        input_arr
-    ).float().unsqueeze(0).unsqueeze(0)
+    input_tensor = torch.from_numpy(input_arr).float()
+    input_tensor = input_tensor.unsqueeze(0).unsqueeze(0)
+
+    transform = transforms.Compose([
+        transforms.Resize(
+            (CONFIG.common.input_size, CONFIG.common.input_size)),
+    ])
+
     input_tensor = transform(input_tensor)
     with torch.no_grad():
         score = model(input_tensor.to(device)).to('cpu').item()
@@ -67,13 +65,16 @@ def gpu_inference(model, sound_path, n_mfcc):
     for i in tqdm(range(n_mfcc)):
         start_segment = i * CONFIG.inference.window_wid
 
-        spec = get_melspectrogram_db(
-            sound_path, offset=start_segment,
-            duration=CONFIG.inference.window_wid
-        )
+        spec = get_melspectrogram_db(sound_path,
+                                     offset=start_segment,
+                                     duration=CONFIG.inference.window_wid)
         input_arr = spec_to_image(spec)
         input_tensor = torch.from_numpy(input_arr)  # 2D tensor
         input_tensor = input_tensor.unsqueeze(0).unsqueeze(0)
+        transform = transforms.Compose([
+            transforms.Resize(
+                (CONFIG.common.input_size, CONFIG.common.input_size)),
+        ])
         input_tensor = transform(input_tensor)
         inputs = torch.cat([inputs, input_tensor], dim=0)
 
@@ -85,7 +86,7 @@ def gpu_inference(model, sound_path, n_mfcc):
     return outputs.squeeze().tolist()
 
 
-def main(sound_path=None):
+def main(sound_path=None) -> float:
     start_time = time.time()
     warnings.filterwarnings('ignore')
     set_seed(CONFIG.inference.seed)
@@ -96,12 +97,11 @@ def main(sound_path=None):
     file_name = os.path.splitext(os.path.basename(sound_path))[0]
     length = mutagen_length(sound_path)
 
-    writer = SummaryWriter(
-        f'{CONFIG.inference.log_dir}/{CONFIG.common.version}/{file_name}'
-    )
+    version = f'{CONFIG.version.data}-{CONFIG.version.code}-{CONFIG.version.param}'
+    writer = SummaryWriter(f'{CONFIG.inference.log_dir}/{version}/{file_name}')
 
     model = get_model(CONFIG.common.arch).to(device)
-    model_path = f'{CONFIG.common.model_dir}/{CONFIG.common.version}.pth'
+    model_path = f'{CONFIG.common.model_dir}/{version}.pth'
     model.load_state_dict(torch.load(model_path))
     model.eval()
 
