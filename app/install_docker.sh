43b183ff8fa3f8c4d96c3a31b399cb2ee32138fc
smygw <smygw72@gmail.com>
2021-06-06 09:52:22 +0900

    added

---
+++ app/install_docker.sh
@@ -0,0 +1,13 @@
+# ubunt
+sudo apt install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key  add -
+sudo add-apt-repository \
+    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) \
+    stable"
+sudo apt install docker-ce
