3c11b6f48049114b8a39c81b26673d99c8189c83
smygw <smygw72@gmail.com>
2021-06-06 09:22:19 +0900

    updated

--- app/setup_cpu.sh
+++ app/setup_cpu.sh
@@ -1,2 +1,3 @@
 docker build -f Dockerfile.CPU -t psa_cpu .
 docker container run -d --rm --ipc=host --name psa_cpu psa_cpu
+docker container exec psa_cpu bash -c "python /app/inference/main.py"
--- app/setup_lambda.sh
+++ app/setup_lambda.sh
@@ -1,11 +1,14 @@
 mkdir -p ~/.aws-lambda-rie && \
-        curl -Lo ~/.aws-lambda-rie/aws-lambda-rie \
-        https://github.com/aws/aws-lambda-runtime-interface-emulator/releases/latest/download/aws-lambda-rie && \
-        chmod +x ~/.aws-lambda-rie/aws-lambda-rie
+    curl -Lo ~/.aws-lambda-rie/aws-lambda-rie \
+    https://github.com/aws/aws-lambda-runtime-interface-emulator/releases/latest/download/aws-lambda-rie && \
+    chmod +x ~/.aws-lambda-rie/aws-lambda-rie
 
 docker build -f Dockerfile.Lambda -t psa_lambda .
+
 docker run --name psa_lambda --rm -p 9000:8080 \
             -v ~/.aws-lambda-rie:/aws-lambda \
             --entrypoint /aws-lambda/aws-lambda-rie \
-            psa \
+            psa_lambda \
             /usr/local/bin/python -m awslambdaric app.handler
+
+curl -XPOST "http://localhost:9000/2015-03-31/functions/function/invocations" -d '{"dummy": "a"}'
