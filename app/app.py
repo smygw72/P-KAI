4100ce276caa1bfbe213a8fa817c4497ea6150b7
smygw <smygw72@gmail.com>
2021-06-26 15:16:35 +0900

    worked in local env (Windows, Debian)

--- app/app.py
+++ app/app.py
@@ -2,38 +2,44 @@ import json
 import boto3
 import base64
 import re
-import json
 import random
 
-def lambda_handler(event, context):
-    
-    # base64 decode
-    jsons = base64.b64decode(event['body-json']).split(b'\r\n')
-    # print(base64.b64decode(event['body-json']))
-    
-    # get filename
-    filename = repr(jsons[1])
-    filename = re.search(r'filename=".*"', filename).group()
-    filename = filename.replace('filename=','')
-    filename = filename.replace('"','')
+from inference.main import main as inference
 
-    # get audio binary data
-    boundary = jsons[0].replace(b'-', b'')
-    audioBody = jsons[4]
-    for i in range(5, len(jsons)):
-        if jsons[i].find(boundary) != -1:
-            break
-        else:
-            audioBody = audioBody + b'\r\n' + jsons[i]
 
-    # s3 update
-    s3 = boto3.resource('s3')
-    bucket = s3.Bucket('test-multipart-request')
-    bucket.put_object(
-        Body = audioBody,
-        Key = filename
-    )
-    
+def handler(event, context):
+
+    print("******lambda event happended******")
+
+    # # base64 decode
+    # jsons = base64.b64decode(event['body-json']).split(b'\r\n')
+    # # print(base64.b64decode(event['body-json']))
+
+    # # get filename
+    # filename = repr(jsons[1])
+    # filename = re.search(r'filename=".*"', filename).group()
+    # filename = filename.replace('filename=', '')
+    # filename = filename.replace('"', '')
+
+    # # get audio binary data
+    # boundary = jsons[0].replace(b'-', b'')
+    # audioBody = jsons[4]
+    # for i in range(5, len(jsons)):
+    #     if jsons[i].find(boundary) != -1:
+    #         break
+    #     else:
+    #         audioBody = audioBody + b'\r\n' + jsons[i]
+
+    # # s3 update
+    # s3 = boto3.resource('s3')
+    # bucket = s3.Bucket('test-multipart-request')
+    # bucket.put_object(
+    #     Body=audioBody,
+    #     Key=filename
+    # )
+
+    audioBody = None
+
     # exec model
     score = exec_model(audioBody)
 
@@ -44,17 +50,12 @@ def lambda_handler(event, context):
 
     return {
         # 'isBase64Encoded': False,
-        # 'statusCode': 200,
+        'statusCode': 200,
         # 'headers':{},
         'body': json.dumps(data)
     }
 
-# dummy function for call model
+
 def exec_model(audio):
-    
-    # dummy score
-    score = random.randint(0, 100)
-    
+    score = inference()
     return score
-    
-    
