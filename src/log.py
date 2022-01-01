a90a70a6d071750b19cfaa436e7dc917c27d44a3
smygw72 <smygw72@gmail.com>
2022-01-01 08:01:39 +0000

    MLflowを削除

--- src/log.py
+++ src/log.py
@@ -1,8 +1,3 @@
-from omegaconf import DictConfig, ListConfig
-import mlflow
-from mlflow.tracking import MlflowClient
-
-
 class AverageMeter(object):
     """Compute and stores the average and current value"""
 
@@ -20,51 +15,6 @@ class AverageMeter(object):
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
-
-
-class MlflowWriter():
-    def __init__(self, experiment_name, **kwargs):
-        self.client = MlflowClient(**kwargs)
-        try:
-            self.experiment_id = self.client.create_experiment(experiment_name)
-        except:
-            self.experiment_id = self.client.get_experiment_by_name(
-                experiment_name).experiment_id
-
-        self.run_id = self.client.create_run(self.experiment_id).info.run_id
-
-    def log_params_from_omegaconf_dict(self, params):
-        for param_name, element in params.items():
-            self._explore_recursive(param_name, element)
-
-    def _explore_recursive(self, parent_name, element):
-        if isinstance(element, DictConfig):
-            for k, v in element.items():
-                if isinstance(v, DictConfig) or isinstance(v, ListConfig):
-                    self._explore_recursive(f'{parent_name}.{k}', v)
-                else:
-                    self.client.log_param(self.run_id, f'{parent_name}.{k}', v)
-        elif isinstance(element, ListConfig):
-            for i, v in enumerate(element):
-                self.client.log_param(self.run_id, f'{parent_name}.{i}', v)
-
-    def log_torch_model(self, model):
-        with mlflow.start_run(self.run_id):
-            mlflow.pytorch.log_state_dict(
-                model.to('cpu').state_dict(), 'models'
-            )
-
-    def log_param(self, key, value):
-        self.client.log_param(self.run_id, key, value)
-
-    def log_metric(self, key, value, epoch):
-        self.client.log_metric(self.run_id, key, value, epoch)
-
-    def log_artifact(self, local_path):
-        self.client.log_artifact(self.run_id, local_path)
-
-    def set_terminated(self):
-        self.client.set_terminated(self.run_id)
 
 
 def update_av_meters(av_meters, meters, sizes):
@@ -88,11 +38,10 @@ def update_writers(tb_writer, ml_writer, av_meters, train_or_test, epoch):
 
     def add(key, value):
         tb_writer.add_scalar(key, value, epoch)
-        ml_writer.log_metric(key, value, epoch)
 
-    # add(f'{train_or_test}/total_acc', av_meters['total_acc'].avg)
+    add(f'{train_or_test}/total_acc', av_meters['total_acc'].avg)
     add(f'{train_or_test}/dif_acc', av_meters['dif_acc'].avg)
-    # add(f'{train_or_test}/sim_acc', av_meters['sim_acc'].avg)
+    add(f'{train_or_test}/sim_acc', av_meters['sim_acc'].avg)
     add(f'{train_or_test}/total_loss', av_meters['total_loss'].avg)
     add(f'{train_or_test}/dif_loss', av_meters['dif_loss'].avg)
     add(f'{train_or_test}/sim_loss', av_meters['sim_loss'].avg)
