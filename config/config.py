4a8691b51da6f38070d5e5abcf82bebf1639bc37
smygw72 <smygw72@gmail.com>
2022-01-04 17:04:28 +0900

    addictとyamlによるパラメータ管理に移行

--- config/config.py
+++ config/config.py
@@ -2,31 +2,15 @@
 import argparse
 import sys
 import os
-from hydra import compose, initialize_config_dir
+import yaml
+from addict import Dict
 
+def get_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config', default='./config/default.yaml')
+    args = parser.parse_args()
+    config = Dict(yaml.safe_load(open(args.config)))
+    config.path = args.config
+    return config
 
-class Config():
-    """
-    hydraによる設定値の取得 (conf)
-    """
-    @staticmethod
-    def get_cnf():
-        """
-        設定値の辞書を取得
-        @return
-            cnf: OmegaDict
-        """
-        parser = argparse.ArgumentParser()
-        parser.add_argument('--config', default='default.yaml')
-        args = parser.parse_args()
-
-        conf_dir = os.path.join(os.getcwd(), "./config/")
-        if not os.path.isdir(conf_dir):
-            print(f"Can not find file: {conf_dir}.")
-            sys.exit(-1)
-
-        with initialize_config_dir(config_dir=conf_dir):
-            cnf = compose(config_name=args.config)
-            return cnf
-
-CONFIG = Config.get_cnf()
+CONFIG = get_config()
